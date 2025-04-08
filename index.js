ac6f1a188f4f5ab0620928e81324d47b5f3350db
Ruan Gomes <143746245+Ruannes@users.noreply.github.com>
2025-04-08 16:59:15 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { Sequelize, DataTypes } from "sequelize";
 
 const sequelize = new Sequelize('BD2', 'postgres', 'postgres', {
     host: 'localhost',
@@ -12,6 +12,51 @@ async function conectar(){
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
       } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        const User = sequelize.define(
+          'User',
+          {
+            // Model attributes are defined here
+            firstName: {
+              type: DataTypes.STRING,
+              allowNull: false,
+            },
+            lastName: {
+              type: DataTypes.STRING,
+              // allowNull defaults to true
+            },
+          },
+          {
+            // Other model options go here
+          },
+        );
+          console.error('Unable to connect to the database:', error);
       }
+}
+
+const User = sequelize.define(
+  'User',
+  {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      // allowNull defaults to true
+    },
+  },
+  {
+    // Other model options go here
+  },
+);
+User.sync();
+
+salvar();
+async function salvar() {
+  const user = await User.create({
+    firstName: 'Ruan',
+    lastName: 'Gomez'
+  });
+  console.log('Usuário salvo com sucesso!');
 }
